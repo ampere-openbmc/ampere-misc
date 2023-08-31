68ca0d7b4058e6b2847864a10c29d07f8058ba00
Thu Nguyen <thu@os.amperecomputing.com>
2023-08-31 17:52:42 +0700

    Remove mctp-ctrl application
    Remove mctp-ctrl application as it is no longer needed when switching to
    MCTP in kernel approach.
    
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- subprojects/mctp-ctrl/ampere_s1_ready.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This is dummy file to detect S1 ready or not
-echo 0
-
--- subprojects/mctp-ctrl/ampere_slave_present.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This is dummy file to indicate slave is presenst
-# Users have to override this file
-echo 0
-
