f67b6e90fa6507403581e1dee249d6002c5b8f77
Thu Nguyen <thu@os.amperecomputing.com>
2023-11-08 16:50:55 +0700

    ampere-cpu-present: remove unnescessary codes
    CPU dbus' present property identifies the installation status of the
    CPU, not the available status. So, this property should not be changed
    when the Host transitions to OFF or ON.
    This commit removes the redundant codes so that the property correctly
    reflects the CPU present status.
    
    Tested:
    1. Reboot BMC. Check CPU present D-Bus property.
    `busctl tree xyz.openbmc_project.Ampere.Cpu`
    2. Power off the host. Check CPU present property.
    `busctl tree xyz.openbmc_project.Ampere.Cpu`
    
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- subprojects/ampere-cpu-present/ampereCpuPresent.cpp
+++ subprojects/ampere-cpu-present/ampereCpuPresent.cpp
@@ -36,7 +36,6 @@
 #include <thread>
 
 bool addSoCInterfaces = false;
-bool s1_ready = false;
 // setup connection to dbus
 boost::asio::io_service io;
 auto conn = std::make_shared<sdbusplus::asio::connection>(io);
@@ -51,21 +50,6 @@ const char *script_s1_present = "/usr/sbin/ampere_utils host present s1";
 constexpr const char *cpuInventoryPath =
 	"/xyz/openbmc_project/inventory/system/chassis/motherboard";
 
-constexpr const char *hostStatePath = "/xyz/openbmc_project/state/host0";
-constexpr const char *hostStateInterface = "xyz.openbmc_project.State.Host";
-constexpr const char *hostStateProp = "CurrentHostState";
-constexpr const char *hostStateOff =
-	"xyz.openbmc_project.State.Host.HostState.Off";
-constexpr const char *hostStateOn =
-	"xyz.openbmc_project.State.Host.HostState.On";
-constexpr const char *hostStateRunning =
-	"xyz.openbmc_project.State.Host.HostState.Running";
-
-constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
-constexpr auto MAPPER_PATH = "/xyz/openbmc_project/object_mapper";
-constexpr auto MAPPER_INTERFACE = "xyz.openbmc_project.ObjectMapper";
-constexpr auto PROPERTY_INTERFACE = "org.freedesktop.DBus.Properties";
-
 std::string exec(const char *cmd)
 {
 	std::array<char, 128> buffer;
@@ -79,66 +63,6 @@ std::string exec(const char *cmd)
 		result += buffer.data();
 	}
 	return result;
-}
-
-std::string getService(sdbusplus::bus_t &bus, std::string path,
-		       std::string interface)
-{
-	auto mapper = bus.new_method_call(MAPPER_BUSNAME, MAPPER_PATH,
-					  MAPPER_INTERFACE, "GetObject");
-
-	mapper.append(path, std::vector<std::string>({ interface }));
-
-	std::vector<std::pair<std::string, std::vector<std::string> > >
-		mapperResponse;
-
-	try {
-		auto mapperResponseMsg = bus.call(mapper);
-
-		mapperResponseMsg.read(mapperResponse);
-		if (mapperResponse.empty()) {
-			std::cerr << "Error no matching service " << std::endl;
-			return "";
-		}
-	} catch (const sdbusplus::exception_t &e) {
-		std::cerr << "Error no matching service " << std::endl;
-		return "";
-	}
-
-	return mapperResponse.begin()->first;
-}
-
-std::string getProperty(sdbusplus::bus_t &bus, const std::string &path,
-			const std::string &interface,
-			const std::string &propertyName)
-{
-	std::variant<std::string> property;
-	std::string service = getService(bus, path, interface);
-	if (service == "") {
-		return "";
-	}
-
-	auto method = bus.new_method_call(service.c_str(), path.c_str(),
-					  PROPERTY_INTERFACE, "Get");
-
-	method.append(interface, propertyName);
-
-	try {
-		auto reply = bus.call(method);
-		reply.read(property);
-	} catch (const sdbusplus::exception_t &e) {
-		std::cerr << "Error in property Get " << propertyName
-			  << std::endl;
-		throw;
-	}
-
-	if (std::get<std::string>(property).empty()) {
-		std::cerr << "Error reading property response for "
-			  << propertyName << std::endl;
-		throw std::runtime_error("Error reading property response");
-	}
-
-	return std::get<std::string>(property);
 }
 
 void checkCpuPresent()
@@ -200,86 +124,11 @@ void addCpuPresentInterfaces()
 	}
 }
 
-void removeCpuPresentInterfaces()
-{
-	if (addSoCInterfaces) {
-		try {
-			if (s0_present && hostPresenceS0 != nullptr) {
-				std::cerr << "Removing interface S0"
-					  << std::endl;
-				objectServer.remove_interface(hostPresenceS0);
-				hostPresenceS0 = nullptr;
-			}
-
-			if (s1_present && hostPresenceS1 != nullptr) {
-				std::cerr << "Removing interface S1"
-					  << std::endl;
-				objectServer.remove_interface(hostPresenceS1);
-				hostPresenceS1 = nullptr;
-			}
-			addSoCInterfaces = false;
-		} catch (const std::exception &e) {
-			std::cerr << e.what() << '\n';
-		}
-	}
-}
-
-sdbusplus::bus::match::match
-startHostStateMonitor(std::shared_ptr<sdbusplus::asio::connection> conn)
-{
-	auto startEventMatcherCallback = [](sdbusplus::message::message &msg) {
-		boost::container::flat_map<std::string,
-					   std::variant<std::string> >
-			propertiesChanged;
-		std::string interfaceName;
-
-		msg.read(interfaceName, propertiesChanged);
-		if (propertiesChanged.empty()) {
-			return;
-		}
-
-		std::string event = propertiesChanged.begin()->first;
-		auto variant = std::get_if<std::string>(
-			&propertiesChanged.begin()->second);
-
-		if (event.empty() || variant == nullptr) {
-			return;
-		}
-		if (event == hostStateProp) {
-			if (*variant == hostStateRunning) {
-				addCpuPresentInterfaces();
-			} else {
-				removeCpuPresentInterfaces();
-			}
-		}
-	};
-
-	sdbusplus::bus::match::match startEventMatcher(
-		static_cast<sdbusplus::bus::bus &>(*conn),
-		"type='signal',interface='org.freedesktop.DBus.Properties',member='"
-		"PropertiesChanged',arg0namespace='xyz.openbmc_project.State.Host'",
-		std::move(startEventMatcherCallback));
-
-	return startEventMatcher;
-}
-
 int main(int argc, char **argv)
 {
 	conn->request_name("xyz.openbmc_project.Ampere.Cpu");
 	objectServer.add_manager("/xyz/openbmc_project/inventory");
-
-	auto state =
-		getProperty(static_cast<sdbusplus::bus::bus &>(*conn),
-			    hostStatePath, hostStateInterface, hostStateProp);
-	if (state != "") {
-		if (state == hostStateRunning) {
-			addCpuPresentInterfaces();
-		} else {
-			removeCpuPresentInterfaces();
-		}
-	}
-
-	sdbusplus::bus::match::match hostStateMon = startHostStateMonitor(conn);
+	addCpuPresentInterfaces();
 	io.run();
 
 	return 0;
